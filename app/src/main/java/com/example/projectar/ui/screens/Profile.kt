d512b03126806061f340fdf14da4237db2d9cfcb
taneli1 <leevi.limnell@gmail.com>
2021-10-06 13:31:18 +0300

    Fixed merge bug

--- app/src/main/java/com/example/projectar/ui/screens/Profile.kt
+++ app/src/main/java/com/example/projectar/ui/screens/Profile.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.projectar.ui.components.TopBar
 import com.example.projectar.ui.theme.Orange
 import com.example.projectar.ui.theme.DarkGrey
 
