5578cca6d15befed9891c5e7cd909b87fed1cae5
taneli1 <leevi.limnell@gmail.com>
2021-10-13 18:00:43 +0300

    lamps added

--- app/src/main/java/com/example/projectar/data/appdata/products/ProductList.kt
+++ app/src/main/java/com/example/projectar/data/appdata/products/ProductList.kt
@@ -49,11 +49,40 @@ object ProductList {
 
         )
 
-    private val sofas = mutableListOf<Product>()
+    private val lamps = mutableListOf<Product>(
+        createProduct(
+            "Red lamp",
+            "Nice lamp for tables.",
+            10f,
+            "lamp/1/lamp.jpg",
+            "lamp/1/lamp.gltf"
+        ),
+        createProduct(
+            "Black lamp",
+            "Nice lamp for tables.",
+            12.30f,
+            "lamp/2/lamp2.jpg",
+            "lamp/2/lamp2.gltf"
+        ),
+        createProduct(
+            "Exotic lamp",
+            "Good nice nice.",
+            30f,
+            "lamp/3/lamp3.jpg",
+            "lamp/3/lamp3.gltf"
+        ),
+        createProduct(
+            "Table lamp",
+            "Nice lamp for tables.",
+            9.98f,
+            "lamp/4/lamp4.jpg",
+            "lamp/4/lamp4.gltf"
+        ),
+    )
 
     val data: List<Product> = mutableListOf<Product>().apply {
         addAll(tables)
-        addAll(sofas)
+        addAll(lamps)
     }
 
     private fun createProduct(
